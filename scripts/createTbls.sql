f6c12fcb47037e79a55f6e3bd73f9c2ab4b281b3
Kleo <kleo@hasani>
2021-09-23 21:26:50 -0400

    added init scripts, made some settings changes and added some boilerplate code

---
+++ scripts/createTbls.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS public."tblUsers"
+(
+    id serial NOT NULL,
+    username character varying(256) NOT NULL,
+    password character varying(256) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS public."tblMovies"
+(
+    id serial NOT NULL,
+    user_id serial NOT NULL,
+    title text NOT NULL,
+    subtitle text,
+    description text,
+    art bytea[],
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id)
+        REFERENCES public."tblUsers" (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID
+);
---
+++ scripts/init.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+set -e
+
+echo "|-----------------------------|"
+echo "|  Welcome to CINEMA STAND    |"
+echo $'|-----------------------------|\n'
+
+  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    read -p 'User: ' db_user
+    read -p 'Host: ' db_host
+    read -p 'Port: ' db_port
+    read -sp "Password: " db_pass
+
+    echo $'\nCreating Database\n'
+
+    #psql -d postgres -U $db_user -h $db_host -p $db_port -W -f ./init.sql
+
+    psql postgresql://$db_user:$db_pass@$db_host:$db_port/postgres -f ./createDb.sql
+    
+    echo $'\nCreating Tables\n'
+
+    psql postgresql://$db_user:$db_pass@$db_host:$db_port/cinemastand -f ./createTbls.sql
+
+    echo $'\n|-------------|'
+    echo "|  All done   |"
+    echo "|-------------|"
+
+  else
+    echo "Unsupported OS"
+  fi
+
+exit
