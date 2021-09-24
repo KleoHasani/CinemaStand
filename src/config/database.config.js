f603f30aeda9adf4af24b3a5ca8097abd2fabd4b
Kleo <kleo@hasani>
2021-09-23 22:15:58 -0400

    updated db config

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -1,27 +1,14 @@
 const { Pool } = require("pg");
 
-function init() {
-  const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_DATABASE,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    idleTimeoutMillis: 30000,
-    connectionTimeoutMillis: 2000,
-    max: 20,
-  });
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
+  user: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+  max: 20,
+});
 
-  pool.connect((error, client, done) => {
-    if (error) throw error;
-    client.query("", (err, res) => {
-      if (err) throw err;
-      console.log(res);
-      done();
-    });
-  });
-
-  return pool;
-}
-
-module.exports = { init };
+module.exports = { pool };
--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
-const { config } = require("dotenv");
+require("dotenv").config();
 const express = require("express");
-const { init } = require("./config/database.config");
 
-config();
+const { pool } = require("./config/database.config");
 
 const HOST = process.env.HOST || "localhost";
 const PORT = parseInt(process.env.PORT, 10) || 8080;
@@ -11,8 +10,6 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-init();
 
 app.listen(PORT, () => {
   console.info(`API is live on http://${HOST}:${PORT}`);
